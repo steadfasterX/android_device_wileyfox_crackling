6b617520c98d48aa372e97a55e59b573e6f2d5d9
stay steadfast <steadfasterX@users.noreply.github.com>
2020-09-17 16:30:23 +0200

    crackling: add vendorsetup for eOS

---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+############# /e/ section ###############
+export EOS_BRANCH_NAME="v1-pie"
+export EOS_RELEASE_TYPE="UNOFFICIAL"
+export EOS_OTA_CLEARTEXT=true
+export EOS_USE_CCACHE=true
+export EOS_CCACHE_DIR=/tmp/ccache
+export EOS_CCACHE_SIZE=14G
+export EOS_TMP_DIR=/tmp/eos
+export EOS_ZIP_DIR=/tmp/eos/zips
